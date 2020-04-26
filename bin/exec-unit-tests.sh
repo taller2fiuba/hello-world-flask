5e4a6930a2828ec21d4fe427e9b1468af554a4de
Franco Liberali <franco.liberali@gmail.com>
2020-04-25 22:49:37 -0300

    meter coverage y coveralls

--- bin/exec-unit-tests.sh
+++ bin/exec-unit-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -name "*.py" | xargs pylint --load-plugins pylint_flask --ignore wsgi.py && nose2 -v
+find . -type f -name "*.py" | xargs pylint --load-plugins pylint_flask --ignore wsgi.py && coverage run -m nose2 -v && coverage report -m
