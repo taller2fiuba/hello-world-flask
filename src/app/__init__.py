531bf1055a8946ecc88a7df03e465b7a719fd86a
Lucho <sportelliluciano@gmail.com>
2020-04-29 17:58:50 -0300

    Agrego migration base y configuraciones

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,8 +2,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import requests
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 API = Api(app)
 
 
@@ -19,3 +25,5 @@ API.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+from app import models
