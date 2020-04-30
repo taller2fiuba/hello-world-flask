f58db26e28d4a7659278e927cddebc979b92b66a
Lucho <sportelliluciano@gmail.com>
2020-04-30 16:00:54 -0300

    Arreglo estilo

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,13 +1,9 @@
 import unittest
-import mock
 
 from app.models import User
 
 
 class ModelTestCase(unittest.TestCase):
-
     def test_user_repr(self):
-        u = User(username='Lucho', email='lucho@lucho.lucho')
-        self.assertEqual(repr(u), '<User Lucho>')
-
-
+        user = User(username='Lucho', email='lucho@lucho.lucho')
+        self.assertEqual(repr(user), '<User Lucho>')
